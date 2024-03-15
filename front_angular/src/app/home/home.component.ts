dd15e906dec4b83b3abf5f060bd48caf13e972c4
Vitali Pantsialei <vitalbit@tut.by>
2024-03-15 21:44:14 +0300

    Return back CSS usage

--- front_angular/src/app/home/home.component.ts
+++ front_angular/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
